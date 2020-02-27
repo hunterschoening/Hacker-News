d9cdb0715135a9f30b7fd155022b1f17ca80ba9c
Hunter Schoening <hunter.schoening@gmail.com>
2020-02-27 14:45:46 -0700

    Initial Commit

---
+++ Hacker News (SwiftUI)Tests/Hacker_News__SwiftUI_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Hacker_News__SwiftUI_Tests.swift
+//  Hacker News (SwiftUI)Tests
+//
+//  Created by Hunter Schoening on 2/27/20.
+//  Copyright © 2020 Hunter Schoening. All rights reserved.
+//
+
+import XCTest
+@testable import Hacker_News__SwiftUI_
+
+class Hacker_News__SwiftUI_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
